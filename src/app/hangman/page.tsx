cddef7bfdfb52c1b3c2a26105c91661d1dc92b64
JannikU09 <j.uphoff@innfactory.de>
2025-12-02 09:35:41 +0100

    feat: Hinzufügen der Komponente Page, um die Zeit, die Eingabe und die Tabelle für die falschen Buchstaben, anzuzeigen.

---
+++ src/app/hangman/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import React from "react";
+
+import { Words } from "@/src/components/words/words";
+import { Title } from "@/src/components/title/title";
+import { Eingabe } from "@/src/components/eingabe/eingabe";
+import { Stoppuhr } from "@/src/components/stoppuhr/stoppuhr";
+import { BuchstabenTable } from "@/src/components/eingabe/buchstabenTable";
+
+export default function Page() {
+    return (
+        <>
+            <Title title="Hangman" size="bigTitle" />
+
+            <Stoppuhr/>
+            <Words />
+            <Eingabe />
+            <BuchstabenTable />
+        </>
+    )
+}
