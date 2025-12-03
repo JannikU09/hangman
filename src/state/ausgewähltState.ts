addaafde06989fe295bf8b4b6ef8b5bda75319eb
JannikU09 <j.uphoff@innfactory.de>
2025-12-03 15:44:18 +0100

    feat: Ändern des verwendeten Atoms zu "atomWithStorage", um den State zu halten nachdem der Tab geschlossen wurde.

--- src/state/ausgewähltState.ts
+++ src/state/ausgewähltState.ts
@@ -1,3 +1,3 @@
-import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
-export const ausgewähltState = atom<string>('de');
+export const ausgewähltState = atomWithStorage('ausgewähltState', <string>("de"));
