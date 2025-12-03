6bde8757a6f95d31769fc9716fcad2be41b6bff7
JannikU09 <j.uphoff@innfactory.de>
2025-12-03 08:10:48 +0100

    feat: Hinzufügen des Imports für die Auswahl der Sprache.

--- src/app/hangman/page.tsx
+++ src/app/hangman/page.tsx
@@ -7,6 +7,7 @@ import { Title } from "@/src/components/title/title";
 import { Eingabe } from "@/src/components/eingabe/eingabe";
 import { Stoppuhr } from "@/src/components/stoppuhr/stoppuhr";
 import { BuchstabenTable } from "@/src/components/eingabe/buchstabenTable";
+import SprachenAuswahl from "@/src/components/sprache/sprachenAuswahl";
 
 export default function Page() {
     return (
@@ -17,6 +18,7 @@ export default function Page() {
             <Words />
             <Eingabe />
             <BuchstabenTable />
+            <SprachenAuswahl/>
         </>
     )
 }
