c0db57ab8c59f255017178c53836c3087eadab22
JannikU09 <j.uphoff@innfactory.de>
2025-12-03 15:51:24 +0100

    feat: Ändern des verwendeten Atoms zu "atomWithStorage", um den State zu halten, nachdem der Tab geschlossen wurde.

--- src/state/splittedWortState.ts
+++ src/state/splittedWortState.ts
@@ -1,3 +1,3 @@
-import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
-export const splittedWortState = atom<string[]>([]);
+export const splittedWortState = atomWithStorage("splittedWortState", <string[]>([]));
