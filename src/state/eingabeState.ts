0d138d150c2c7e16d0781e1cc4ab43a6c1cccecb
JannikU09 <j.uphoff@innfactory.de>
2025-12-03 15:47:49 +0100

    feat: Ändern des verwendeten Atoms zu "atomWithStorage", um den State zu halten, nachdem der Tab geschlossen wurde.

--- src/state/eingabeState.ts
+++ src/state/eingabeState.ts
@@ -1,3 +1,4 @@
-import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
-export const eingabeState = atom<string>("");
+
+export const eingabeState = atomWithStorage("eingabeState", <string>(""));
