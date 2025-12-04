dda4bb6628dcdf5e85a1782ad29b1d2368c50e58
JannikU09 <j.uphoff@innfactory.de>
2025-12-04 15:43:10 +0100

    feat: Hinzufügen der Implementierung für die Reset-Funktion.

--- src/app/hangman/page.tsx
+++ src/app/hangman/page.tsx
@@ -6,7 +6,8 @@ import { Words } from "@/src/components/words/words";
 import { Title } from "@/src/components/title/title";
 import { Eingabe } from "@/src/components/eingabe/eingabe";
 import { Stoppuhr } from "@/src/components/stoppuhr/stoppuhr";
-import { BuchstabenTable } from "@/src/components/eingabe/buchstabenTable";
+import { BuchstabenTable } from "@/src/components/table/buchstabenTable";
+import { Reset } from "@/src/components/reset/reset";
 import SprachenAuswahl from "@/src/components/sprache/sprachenAuswahl";
 
 export default function Page() {
@@ -17,6 +18,7 @@ export default function Page() {
             <Stoppuhr/>
             <Words />
             <Eingabe />
+            <Reset/>
             <BuchstabenTable />
             <SprachenAuswahl/>
         </>
