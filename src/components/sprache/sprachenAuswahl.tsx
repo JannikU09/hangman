9dcdd522d6e62252ca1742ed6dac96dd911328da
JannikU09 <j.uphoff@innfactory.de>
2025-12-08 10:09:58 +0100

    refactor: Höhe der Sprachauswahl geändert.

--- src/components/sprache/sprachenAuswahl.tsx
+++ src/components/sprache/sprachenAuswahl.tsx
@@ -23,7 +23,7 @@ export default function SprachenAuswahl() {
             <FormControl sx={{
                 width: "150px",
                 margin: "15px",
-                bottom: "560px",
+                bottom: "540px",
                 left: "1225px",
             }}>
                 <InputLabel id="auswahlLable">Sprache</InputLabel>
