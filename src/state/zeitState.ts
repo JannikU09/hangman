4577c5211a1b02ab6f5c2d4e0b28af2103d082c3
JannikU09 <j.uphoff@innfactory.de>
2025-12-09 15:32:10 +0100

    refactor: Hinzufügen des Atoms für die initZeit.

--- src/state/zeitState.ts
+++ src/state/zeitState.ts
@@ -1,3 +1,5 @@
 import { atomWithStorage } from "jotai/utils";
 
 export const zeitState = atomWithStorage("zeitState", <number>(0));
+
+export const initZeitState = atomWithStorage("zeitState",  <number>(0));
