06a32b5adc72b349dec43c10efa55314cfbafc47
JannikU09 <j.uphoff@innfactory.de>
2025-12-11 10:50:24 +0100

    feat: Hinzufügen der if-Funktion, sodass die API nur abgerufen wird, wenn "Neustarten" ausgewählt wird.

--- src/components/words/words.tsx
+++ src/components/words/words.tsx
@@ -1,14 +1,12 @@
 "use client"
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { buchstabeState } from "@/src/state/buchstabeState";
 import { useAtom } from "jotai";
-import { Eingabe } from "../eingabe/eingabe";
 import { wortState } from "@/src/state/wortState";
-import { Title } from "../title/title";
 import { ausgewähltState } from "@/src/state/ausgewähltState";
 import { optionenState } from "@/src/state/optionenState";
-
+import { Title } from "../title/title";
 
 type Wort = {
     word: string,
@@ -20,70 +18,60 @@ type Wort = {
 
 export const Words = () => {
 
+    const [selectedValue, setSelectedValue] = useAtom(optionenState);
     const [wort, setWort] = useAtom(wortState);
-    const [laenge, setLaenge] = useState<number>(0);
-    const [buchstabenList, setBuchstabenList] = useAtom(buchstabeState);
-    const [sprache, setSprache] = useAtom(ausgewähltState);
-    const [selectedValue, setSelectedValue] = useAtom(optionenState)
+    const [sprache, setSprache] = useAtom(ausgewähltState)
 
 
     useEffect(() => {
+        if (selectedValue === "Neustarten" || sprache === "") {
 
-        const superWortFetch = async () => {
-            try {
-                const antwort = await fetch(`https://random-words-api.kushcreates.com/api?language=${sprache}&type=uppercase&words=1`);
-                const daten = await antwort.json() as Wort[];
+            const superWortFetch = async () => {
+                try {
+                    const antwort = await fetch(`https://random-words-api.kushcreates.com/api?language=${sprache}&type=uppercase&words=1`);
+                    const daten = await antwort.json() as Wort[];
 
-                if (daten.length === 0) {
-                    superWortFetch();
-                };
+                    if (daten.length === 0) {
+                        superWortFetch();
+                    };
 
-                console.log("Daten: ", daten);
-                return daten[0]
+                    console.log("Daten: ", daten);
+                    return daten[0];
 
-            } catch (error: unknown) {
-                if (error instanceof Error) {
-                    console.log(error)
-                };
-            };
-        };
-        console.log("Test", selectedValue);
-
-        const superWort = async () => {
-            try {
-                const daten = await superWortFetch();
-                if (!daten || daten.length > 15 || daten.word.includes(" ")) {
-                    await superWort();
-                    return;
-                };
-
-                //Wort und Länge werden gesetzt.
-                setWort(daten.word);
-                setLaenge(daten.length);
-
-
-            } catch (error: unknown) {
-                if (error instanceof Error) {
-                    console.log(error);
+                } catch (error: unknown) {
+                    if (error instanceof Error) {
+                        console.log(error);
+                    };
                 };
             };
-        };
-        superWort();
 
+            const superWort = async () => {
+                try {
+                    const daten = await superWortFetch();
+                    if (!daten || daten.length > 15 || daten.word.includes(" ")) {
+                        await superWort();
+                        return;
+                    };
 
-    }, [sprache, selectedValue]);
+                    //Wort wird gesetzt.
+                    setWort(daten.word);
 
-    useEffect(() => {
-        setWort(wort.replace(/ß/g, "SS"));
-    }, [wort]);
+                    console.log("daten.word: ", daten.word);
+                    console.log("wort: ", wort);
 
-    console.log("Buchstaben: ", buchstabenList);
-    console.log("Wortlänge", laenge);
-
+                } catch (error: unknown) {
+                    if (error instanceof Error) {
+                        console.log(error);
+                    };
+                };
+            };
+            superWort();
+        }
+    }, [selectedValue, sprache])
 
     return (
         <>
-            {/* <Title title={`Wort: ${wort} || Länge: ${laenge}`} size="smallTitle" /> */}
+            <Title title={`Wort: ${wort}`} size="smallTitle" />
         </>
     )
-};
+}
